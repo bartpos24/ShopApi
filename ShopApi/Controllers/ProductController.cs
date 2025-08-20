f43c6a8ea0464e5c2c45c923b51d4a71d39154c5
bartpos24 <bartek.poskart24@gmail.com>
2025-08-20 20:13:02 +0200

    Added test endpoint to ProductController

--- ShopApi/Controllers/ProductController.cs
+++ ShopApi/Controllers/ProductController.cs
@@ -12,34 +12,34 @@ using Microsoft.AspNetCore.Authorization;
 
 namespace ShopApi.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class ProductController : ShopController
-    {
-        private IProductRepository ProductRepository { get; set; }
+	[ApiController]
+	[Route("api/[controller]")]
+	public class ProductController : ShopController
+	{
+		private IProductRepository ProductRepository { get; set; }
 		private readonly IOpenFoodFactsService openFoodFactsService;
 		public ProductController(ShopDbContext context, ILogger<ProductController> logger, IProductRepository productRepository, IOpenFoodFactsService _openFoodFactsService) : base(context, logger)
-        {
-            ProductRepository = productRepository;
+		{
+			ProductRepository = productRepository;
 			openFoodFactsService = _openFoodFactsService;
 		}
 
-        [HttpPost]
-        [Route("[action]")]
+		[HttpPost]
+		[Route("[action]")]
 		[Authorize(Roles = "ADM,USR")]
 		public async Task<ActionResult> InitProductFromCSV()
-        {
-            string filePath = @"F:\Bartek\Projekty\Inwentaryzacja\BarcodeDatabase\OpenFoodFacts\CSV\en.openfoodfacts.org.products.csv";
+		{
+			string filePath = @"F:\Bartek\Projekty\Inwentaryzacja\BarcodeDatabase\OpenFoodFacts\CSV\en.openfoodfacts.org.products.csv";
 
-            var result = await ProductRepository.InitProductFromCSV(filePath);
+			var result = await ProductRepository.InitProductFromCSV(filePath);
 
-            if (result.IsNullOrEmpty())
-                Ok("Inicjalizacja produktów z pliku csv zakończona pomyślnie");
-            else NotFound($"Wystąpił problem podczas inicjalizacji produktów: {result}");
+			if (result.IsNullOrEmpty())
+				Ok("Inicjalizacja produktów z pliku csv zakończona pomyślnie");
+			else NotFound($"Wystąpił problem podczas inicjalizacji produktów: {result}");
 
 
-            return Ok();
-        }
+			return Ok();
+		}
 		[HttpGet]
 		[Route("[action]")]
 		[Authorize(Roles = "ADM,USR")]
@@ -112,62 +112,71 @@ namespace ShopApi.Controllers
 		}
 
 		[HttpGet]
-        [Route("[action]")]
+		[Route("[action]")]
 		[Authorize(Roles = "ADM,USR")]
 		public async Task<ActionResult> GetProductByBarcode([FromQuery] string barcode)
-        {
-            string listOfProduct = "";//var listOfProduct = Context.Barcodes.Where(b => b.Code == barcode).Select(b => b.Product).ToList();
+		{
+			string listOfProduct = "";//var listOfProduct = Context.Barcodes.Where(b => b.Code == barcode).Select(b => b.Product).ToList();
 			if (listOfProduct.IsNullOrEmpty())
-            {
-                var product = await Context.InitProducts.FirstOrDefaultAsync(ip => ip.Barcode == barcode);
-                if(product == null) 
-                    return NotFound("Nie znaleziono produktu z podanym kodem");
-                else
-                {
-                    var productToAdd = new Product()
-                    {
-                        Name = product.Name,
-                        Brand = product.Brand,
-                        Label = product.Label,
-                        Capacity = product.Capacity,
-                        IsGeneral = false,
-                        UnitId = Context.Units.FirstOrDefault(w => w.Code == "szt").Id
-                    };
-                    productToAdd.Barcodes.Add(new Barcode()
-                    {
-                        Code = product.Barcode
-                    });
-                    await Context.Products.AddAsync(productToAdd);
-                    await Context.SaveChangesAsync();
-                    //listOfProduct = Context.Barcodes.Where(b => b.Code == barcode).Select(b => b.Product).ToList();
-                    return listOfProduct.IsNullOrEmpty() ? NotFound("Nie znaleziono produktu z podanym kodem") : Ok(listOfProduct);
-                }
-            } else
-                return Ok(listOfProduct);
-        }
-        [HttpPost]
-        [Route("[action]")]
+			{
+				var product = await Context.InitProducts.FirstOrDefaultAsync(ip => ip.Barcode == barcode);
+				if (product == null)
+					return NotFound("Nie znaleziono produktu z podanym kodem");
+				else
+				{
+					var productToAdd = new Product()
+					{
+						Name = product.Name,
+						Brand = product.Brand,
+						Label = product.Label,
+						Capacity = product.Capacity,
+						IsGeneral = false,
+						UnitId = Context.Units.FirstOrDefault(w => w.Code == "szt").Id
+					};
+					productToAdd.Barcodes.Add(new Barcode()
+					{
+						Code = product.Barcode
+					});
+					await Context.Products.AddAsync(productToAdd);
+					await Context.SaveChangesAsync();
+					//listOfProduct = Context.Barcodes.Where(b => b.Code == barcode).Select(b => b.Product).ToList();
+					return listOfProduct.IsNullOrEmpty() ? NotFound("Nie znaleziono produktu z podanym kodem") : Ok(listOfProduct);
+				}
+			}
+			else
+				return Ok(listOfProduct);
+		}
+		[HttpPost]
+		[Route("[action]")]
 		[Authorize(Roles = "ADM,USR")]
 		public async Task<ActionResult> AddProduct([FromBody] Product product)
-        {
-            if(!product.IsGeneral)
-            {
-                var productVerify = Context.Products.Where(p => p.Barcodes.Any(b => !product.Barcodes.IsNullOrEmpty() && product.Barcodes.FirstOrDefault().Code == b.Code) && !p.IsGeneral);
-                if (!productVerify.IsNullOrEmpty())
-                    return BadRequest("Produkt o takim kodzie kreskowym jest już dodany");
-            }
-            
-            Context.Products.Add(product);
-            await Context.SaveChangesAsync();
-            return Ok(product.Id);
-        }
-        [HttpGet]
-        [Route("[action]")]
+		{
+			if (!product.IsGeneral)
+			{
+				var productVerify = Context.Products.Where(p => p.Barcodes.Any(b => !product.Barcodes.IsNullOrEmpty() && product.Barcodes.FirstOrDefault().Code == b.Code) && !p.IsGeneral);
+				if (!productVerify.IsNullOrEmpty())
+					return BadRequest("Produkt o takim kodzie kreskowym jest już dodany");
+			}
+
+			Context.Products.Add(product);
+			await Context.SaveChangesAsync();
+			return Ok(product.Id);
+		}
+		[HttpGet]
+		[Route("[action]")]
 		[Authorize(Roles = "ADM,USR")]
 		public async Task<ActionResult> GetAllProducts()
-        {
-            var allProducts = await Context.Products.ToListAsync();
-            return allProducts.IsNullOrEmpty() ? NotFound("Nie znaleziono produktów") : Ok(allProducts);
-        }
-    }
+		{
+			var allProducts = await Context.Products.ToListAsync();
+			return allProducts.IsNullOrEmpty() ? NotFound("Nie znaleziono produktów") : Ok(allProducts);
+		}
+		[HttpGet]
+		[Route("[action]")]
+		[Authorize(Roles = "ADM,USR")]
+		public async Task<ActionResult> GetAllProductsTestOpenApiGenerate()
+		{
+			var allProducts = await Context.Products.ToListAsync();
+			return allProducts.IsNullOrEmpty() ? NotFound("Nie znaleziono produktów") : Ok(allProducts);
+		}
+	}
 }
